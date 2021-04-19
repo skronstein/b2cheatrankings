0b026c2e49bdf833b99af96b9f8665d52ac61040
Simon Kronstein <skronstein7@gmail.com>
2021-04-19 13:34:59 -0700

    Add grid to home page

--- home.php
+++ home.php
@@ -2,19 +2,25 @@
 	$title='Burnout 2 Cheat Records Site';
    require('images.php')
 ?>
-<html>
+<link rel="stylesheet" type="text/css" href="style.css">
    <head>
       <title><?php echo $title;?></title>
    </head>
    <body>
    	<h1><?php echo $title;?></h1>
-	<?php
-      $arraylength = 15;
-		for($itr=0 ; $itr < $arraylength ; $itr++){
-         $imageName = $imageArray[$itr] . ".png";
-         echo "<img src = images/" . $imageName . ">";
-      }
-	?>
+      <?php // track icons to select track
+         $arraylength = 15;
+         for($itr=0 ; $itr < $arraylength ; $itr++){
+            $imageName = $imageArray[$itr] . ".png";
+            echo "<img src = images/" . $imageName . ">";
+         }
+      ?>
+      <div class="container">
+         <div class="subcontainer">1</div>
+         <div class="subcontainer">2</div>
+         <div class="subcontainer">3</div>
+         <div class="subcontainer">4</div>
+      </div>
    </body>
 
 </html>
