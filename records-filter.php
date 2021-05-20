e1a68b4a6f11465702af664a3a8c4542ddb85dd1
Simon Kronstein <skronstein7@gmail.com>
2021-05-19 21:33:48 -0700

    after clicking the Direction button, fetch and display records for all categories

--- records-filter.php
+++ records-filter.php
@@ -2,11 +2,16 @@
 include("config/db_connect.php");
 include('records.php');
 
+$category=$_GET['category'];
+if($category == 'best_laps' || $category == 'total_race_times') $order = "ASC";
+else $order = "DESC";
+
 $track=$_GET['track'];
+
 $reverse = $_GET['reverse'];
 if($reverse == 'true') $reverse = 1;
 else $reverse = 0;
 
-outputRecords("best_laps", "ASC", $conn, $track, $reverse);
+outputRecords($category, $order, $conn, $track, $reverse);
 
 exit();
--- track.php
+++ track.php
@@ -12,6 +12,7 @@
       }?>
 <script>
    reverse = false;
+   thestring = '';
    function updateHalf() {
       reverse = !reverse;
       if(reverse) {
@@ -20,14 +21,28 @@
       else {
          document.getElementById('direction').innerHTML = "Direction: Forward";
       }
+
+      thestring = "records-filter.php?track=<?php echo $track?>";
+      thestring += "&reverse=" + reverse;
+      console.log(thestring);
+
+      ajaxFunction("best_laps");
+      ajaxFunction("total_times");
+      ajaxFunction("big_airs");
+   }
+   function updateAll() {
+      updateHalf();
+      ajaxFunction("big_crashes");
+      ajaxFunction("race_crash_totals");
+      ajaxFunction("most_cars_in_crashes");
+   }
+   function ajaxFunction(category) {
       var ajax = new XMLHttpRequest();
-      thestring = "records-filter.php?reverse=" + reverse;
-      thestring += "&track=<?php echo $track?>";
-      ajax.open("GET", thestring, true);
+      ajax.open("GET", thestring + "&category=" + category, true);
       ajax.send();
       ajax.onreadystatechange = function() {
          if(/* this.readystate == 4 && */ this.status == 200) {
-            document.getElementById('best_laps').innerHTML = this.responseText;
+            document.getElementById(category).innerHTML = this.responseText;
          }
       }
    }
@@ -45,7 +60,7 @@
    <div class="trackfilters">
       <div class="filtergrid">
          <div><button class="btn btn-lg btn-primary">Traffic</button></div>
-         <div><button class="btn btn-lg btn-primary" id="direction" onClick='updateHalf()'>Direction: Forward</button></div>
+         <div><button class="btn btn-lg btn-primary" id="direction" onClick='updateAll()'>Direction: Forward</button></div>
          <div><button class="btn btn-lg btn-primary">Out of Bounds</button></div>
          <div><button class="btn btn-lg btn-primary">Codes</button></div>
       </div>
@@ -60,31 +75,31 @@
          </div>
          <div class="subcontainer-tracks">
             <b>Total Race Time</b><br>
-            <table>
+            <table id="total_times">
               <?php outputRecords("total_times", "ASC", $conn, $track, $reverse); ?>
             </table>
          </div>
          <div class="subcontainer-tracks">
             <b>Big Crash</b><br>
-            <table>
+            <table id="big_crashes">
                <?php outputRecords("big_crashes", "DESC", $conn, $track, $reverse); ?>
             </table>
          </div>
          <div class="subcontainer-tracks">
             <b>Race Crash Total</b><br>
-            <table>
+            <table id="race_crash_totals">
                <?php outputRecords("race_crash_totals", "DESC", $conn, $track, $reverse); ?>
             </table>
          </div>
          <div class="subcontainer-tracks">
             <b>Big Air</b><br>
-            <table>
+            <table id="big_airs">
               <?php outputRecords("big_airs", "DESC", $conn, $track, $reverse); ?>
             </table>
          </div>
          <div class="subcontainer-tracks">
             <b>Most Cars in Crash</b><br>
-            <table>
+            <table id="most_cars_in_crashes">
               <?php outputRecords("most_cars_in_crashes", "DESC", $conn, $track, $reverse); ?>
             </table>
          </div>
