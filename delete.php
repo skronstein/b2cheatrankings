d46c69371b11998aa1f4ce52a34cc2772451ca02
Simon Kronstein <skronstein7@gmail.com>
2021-05-26 21:48:20 -0700

    support deleting records from the database

---
+++ delete.php
@@ -0,0 +1,17 @@
+<?php
+require("protect.php");
+include("config/db_connect.php");
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+    if($stmt = $conn->prepare("DELETE FROM best_laps WHERE id = ? LIMIT 1")){
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        echo "ERROR: could not preapre SQL statement.";
+    }
+    $conn->close();
+    header("Location: view.php");
+}
+else header ("Location: view.php");
+?>
