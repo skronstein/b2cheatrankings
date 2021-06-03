8ac3b24f8f1fa6d6bb1c6972c0be73e91e32c145
Simon Kronstein <skronstein7@gmail.com>
2021-06-02 22:20:42 -0700

    admin: use a mysql prepared statement to display the data about a record when editing it

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@
                 return;
             }
             $error = '';
-            print_r($_POST);
+            //print_r($_POST);
             if(isset($_POST['submit'])){
                 //editing existing record; pre-fill the fields with the info for that record
                 $category = htmlentities($_POST['category'], ENT_QUOTES);
@@ -23,25 +23,15 @@
                 }
             }
             //show the (updated) info for the record
-            $sql_command = "SELECT * FROM best_laps WHERE id = $id";
-            $sql_result = mysqli_query($conn, $sql_command);
-            echo $sql_command;
-            $row = mysqli_fetch_all($sql_result, MYSQLI_ASSOC)[0];
-            if(count($row) == 0) echo "No record found with that ID";
+            if($stmt = $conn->prepare("SELECT `score`, `reverse`, `car`, `player`, `system`, `proof`, `date_acheived`, `track_id` FROM best_laps WHERE id = ?")){
+                $stmt->bind_param("i", $id);
+                $stmt->execute();
+                $stmt->bind_result($score, $reverse, $car, $player, $system, $proof, $date_acheived, $track);
+                $stmt->fetch();
+            }
             renderForm(
                 $error, 
-                $_GET['id'],
-                "lap",
-                $row['score'],
-                $row['reverse'],
-                $row['car'],
-                $row['player'],
-                $row['track_id'],
-                $row['system'],
-                $row['proof'],
-                0,//$row['crashToSaveTime'],
-                0,//$row['oob'],
-                $row['date_acheived']
+                $id, "best_laps", $score, $reverse, $car, $player, $track, $system, $proof, $date_acheived
             );
         } else {
             //adding new record; leave fields blank. $_GET['id']
