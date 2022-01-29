a963a19c269685bd31bf56a50b271d13fbf12216
Simon Kronstein <skronstein7@gmail.com>
2022-01-29 07:31:21 -0800

    setup page: minor changes

--- setup.php
+++ setup.php
@@ -13,7 +13,7 @@
 <h2>Required steps</h2>
 
 <p>In the Config menu in Dolphin, <b>check Enable Cheats</b>.</p>
-Right click on the game in Dolphin's main window. Select Properties -> Game Config -> Editor<br>
+Right click on the game in Dolphin's main window. Select Properties -> Game Config -> Editor.<br>
 for the NTSC version of the game, copy and paste the following codes into the text box in User Config, just below Presets:
 <br>
 <br>[ActionReplay_Enabled]
@@ -36,8 +36,8 @@ for the NTSC version of the game, copy and paste the following codes into the te
 
 <h2>Optional steps</h2>
 
-<p>In Burnout 2's cheats menu, use Free Run if you want to disable traffic.<br>
-I'd recommend using this, especially while getting used to driving with the Super Acceleration and Disable Speed Restriction codes.</p>
+<p>In Burnout 2's own cheats menu, use Free Run if you want to disable traffic.<br>
+I'd recommend doing this, especially while getting used to driving with the Super Acceleration and Disable Speed Restriction codes.</p>
 <p>If you want to keep motion blur enabled, right click on the game in Dolphin -> AR Codes -> uncheck Motion Blur Removal.</p>
 <p>If you want to have all the cars unlocked from the start, you can use my <a href="https://gamefaqs.gamespot.com/gamecube/561501-burnout-2-point-of-impact/saves">save file</a>, which has everything unlocked, and has clean scoreboards.
 To use a .gci save file, copy the gci file into your "Dolphin/User/GC/USA/Card A" folder before starting Dolphin.</p>
