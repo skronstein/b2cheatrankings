7d0b3a59708f0d797362e2deb03ff71111c45a2f
Simon Kronstein <skronstein7@gmail.com>
2022-03-03 12:27:53 -0800

    update copyright date

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="wrapper row3">
   <footer id="footer" class="clear">
-    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">b2cheatrankings.com</a></p>
+    <p class="fl_left">Copyright &copy; 2022 - All Rights Reserved - <a href="#">b2cheatrankings.com</a></p>
     <p class="fl_right">main page icons by <a href="https://iconarchive.com/show/100-flat-icons-by-graphicloads.2.html">GraphicLoads</a> | Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
   </footer>
 </div>
